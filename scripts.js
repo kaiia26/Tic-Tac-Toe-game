ff0709addfe0d1e10a9dac666f91ef8545c33c0e
Kaia Perez Hvala <kaiavictoriap@gmail.com>
2024-11-01 19:48:35 -0400

    Implements turn-based functionality

--- scripts.js
+++ scripts.js
@@ -2,3 +2,21 @@ var p1Score = 0;
 var p2Score = 0;
 var tie = 0;
 var turn = 1;
+
+
+
+
+
+$(".box").on("click", function() {
+    if (turn === 1) {
+        $(this).text("X")
+        turn = 2
+    } else {
+        $(this).text("O")
+        turn = 1
+    }
+    $("#turn").text(turn)
+
+    }
+    
+);
