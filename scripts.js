10ffa9030df8051a532b701d1ffa7ab5c1983ea6
Kaia Perez Hvala <kaiavictoriap@gmail.com>
2024-11-02 18:13:17 -0400

    Added tie function and edited player score

--- scripts.js
+++ scripts.js
@@ -5,27 +5,40 @@ var turn = 1;
 
 function checkWin(val) {
     if (
-        $(".row-1 .box").length === 3 ||
-        $(".row-2 .box").length === 3 ||
-        $(".row-3 .box").length === 3 ||
-        $(".col-1.X").length === 3 ||
-        $(".col-2.X").length === 3 ||
-        $(".col-3.X").length === 3 ||
+        $(".row-1 .box" + val).length === 3 ||
+        $(".row-2 .box" + val).length === 3 ||
+        $(".row-3 .box" + val).length === 3 ||
+        $(".col-1.X" + val).length === 3 ||
+        $(".col-2.X" + val).length === 3 ||
+        $(".col-3.X" + val).length === 3 ||
         (
-            $("#1").hasClass("X") &&
-            $("#5").hasClass("X") &&
-            $("#9").hasClass("X")
+            $("#1").hasClass(val) &&
+            $("#5").hasClass(val) &&
+            $("#9").hasClass(val)
         ) ||
         (
-            $("#3").hasClass("X") &&
-            $("#5").hasClass("X") &&
-            $("#7").hasClass("X")
+            $("#3").hasClass(val) &&
+            $("#5").hasClass(val) &&
+            $("#7").hasClass(val)
         )
     ) {
-        alert("Hi");
+        $(".box").empty()
+        $(".X").removeClass("X")
+        $(".O").removeClass("O")
+        return true;
     }
 }
 
+    function checkTie() {
+        if ($(".X").length + $(".O").length === 9) {
+            $(".box").empty()
+            $(".X").removeClass("X")
+            $(".O").removeClass("O")
+            ties++
+            $("#ties").text(ties)
+            return true;
+        }
+    }
 
 
 
@@ -35,12 +48,21 @@ $(".box").on("click", function() {
         $(this).text("X")
         $(this).addClass("X")
         turn = 2
-        checkWin()
+       if (checkWin("X")) {
+        p1Score++
+        $("#p-1 wins").text(p1Score)
+       } else checkTie()
     } else {
         $(this).text("O")
         $(this).addClass("O")
         turn = 1
-        checkWin()
+        if (checkWin("X")) {
+            p2Score++
+            $("#p-2 wins").text(p2Score)
+           
+        } else checkTie()
+
+        
     }
     $("#turn").text(turn)
 
